19dfe044dab59e4c39af05a557bdf52766eff340
TakakuraMako <1361942776@qq.com>
2024-04-08 19:08:28 +0800

    upgrade

--- DataScienceStudy/分类算法/test.py
+++ DataScienceStudy/分类算法/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import math
 import tree
 
-data = pd.read_csv('./DataScienceStudy/西瓜数据集 2.0.csv')
+data = pd.read_csv('./DataScienceStudy/分类算法/西瓜数据集 2.0.csv')
 epsilon = 2
 lables = ['色泽','根蒂','敲声','纹理','脐部','触感']
 lables_count = {'色泽':{},'根蒂':{},'敲声':{},'纹理':{},'脐部':{},'触感':{}}
@@ -31,7 +31,8 @@ def Calcualate_H(data):
         H_D -= i/data.shape[0] * math.log2(i/data.shape[0])
     return H_D
 
-def Calclate_H_D_A():
+def Calclate_H_D_A(lables, lables_count):
+    H_D_A = dict.fromkeys(lables_count,0)
     for i in lables:
         ls = list(lables_count[i].keys())#每一种性状的具体属性
         for x in ls:#每一个具体属性的熵
@@ -43,22 +44,29 @@ def Calclate_H_D_A():
             if p_i != 0:
                 H -= p_i * math.log2(p_i)
             H_D_A[i] += lables_count[i][x]['num']/data.shape[0]*H
+    return H_D_A
 
 while True:
     H_D = Calcualate_H(data)
     lables_count = Calcualate_num(lables, data, lables_count)
-
-    H_D_A = dict.fromkeys(lables_count,0)
+    H_D_A = Calclate_H_D_A(lables, lables_count)
     G_D_A = dict.fromkeys(lables_count,0)
-
-
+    for i in lables:
         G_D_A[i] = H_D - H_D_A[i]
     print(G_D_A)
     insert = max(G_D_A, key=lambda x: G_D_A[x])
     print(insert)
+
+    for i in lables_count[insert]:
+        if lables_count[insert][i]['是'] == 0:
+            print('no'+ i)
+        elif lables_count[insert][i]['否'] == 0:
+            print('yes'+ i)
+
     lables_count.pop(insert)
     lables.remove(insert)
     data.drop(columns=insert)
+    
     for a,x in data.iterrows():
         print(x[insert])
         if x[insert] == '清晰':
@@ -66,6 +74,5 @@ while True:
     print(data)
     if len(lables_count) == 0:
         break
-
 
 
