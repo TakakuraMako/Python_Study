b7d4a8f89d653c8efe55bd46a6b077276f3f64ac
TakakuraMako <1361942776@qq.com>
2024-05-18 21:42:04 +0800

    upgrade

--- 机器学习/机器学习.py
+++ 机器学习/机器学习.py
@@ -10,7 +10,20 @@ print(data.info())
 print(data.shape)
 print(data.describe())
 '''
+
 #和时间有关，向前填充
-print(np.any(data.isnull()))
-#data.fillna(method='ffill')
-#data.fillna(method='bfill')
+data = data.ffill()
+data = data.bfill()
+
+#data = data.iloc[0:2000]
+#拆分时分秒
+
+plt.figure()
+x = np.linspace(0,data.shape[0],data.shape[0])
+y = data['Aggregate']
+plt.plot(x,y)
+plt.xlabel('Time')
+plt.ylabel('Aggregate')
+plt.xticks(x[::100],list(data['Time'][::100]),rotation = 45)
+plt.grid(axis = 'y')
+plt.show()
