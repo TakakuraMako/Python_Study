7c8514be44b207720696f801e5b44304c06c43bf
TakakuraMako <1361942776@qq.com>
2024-05-23 18:54:44 +0800

    upgrade

--- 机器学习/机器学习.py
+++ 机器学习/机器学习.py
@@ -6,6 +6,9 @@ from sklearn.preprocessing import StandardScaler
 import glob
 import os
 import re
+import matplotlib
+
+matplotlib.rc("font",family='MicroSoft YaHei',weight="bold")
 def Data_preprocessing(data):
         #和时间有关，向前填充
     data = data.ffill()
@@ -19,8 +22,24 @@ def Data_preprocessing(data):
     #首尾记录不是完整一天，去除
     data = data.drop(data[data['Day'] == data.iloc[0,-1]].index).reset_index()
     data = data.drop(data[data['Day'] == data.iloc[-1,-1]].index).reset_index()
-    data_new = data.groupby('Day')['Aggregate'].sum().reset_index()
-    return data, data_new
+
+
+    return data
+
+def Max_hour(data):
+    # 提取日期和小时
+    data['hour'] = data['Time'].dt.strftime('%H')
+
+    # 按小时分组并计算每小时的总用电量
+    hourly_sum = data.groupby('hour')['Aggregate'].sum().reset_index()
+
+    # 找出最大负荷时间
+    max_load_hour = hourly_sum.loc[hourly_sum['Aggregate'].idxmax()]
+
+    # 结果
+    max_hour = max_load_hour['hour']
+    max_value = max_load_hour['Aggregate']
+    return max_hour
 
 def Data_Visualize(data_day):
     #每天用电
@@ -54,69 +73,41 @@ print(data.shape)
 print(data.describe())
 '''
 
-'''
+data_new = pd.DataFrame({},index = ['日峰值','日谷值', '日均值', '谷峰差', '最高用电时段','日峰谷差率'])
 for i in range(len(csv_files)):
     data = pd.read_csv(csv_files[i])
-    data, data_new = Data_preprocessing(data)
+    data = Data_preprocessing(data)
     _add = re.search(r"House\d+", csv_files[i].replace('_','')).group()
-    data_day = pd.merge(data_day, data_new, how='outer', on='Day',suffixes=['',_add])
-data_day['Day'] = data_day['Day'].astype(str)
-print(data_day.dtypes)
-data_day.to_csv('./机器学习/data_day.csv',index=False)
-'''
-data_day = pd.read_csv('./机器学习/data_day.csv',converters={u'Day':str})
-print(data_day['Day'])
-Data_Visualize(data_day)
-
-#日最高用电，最低用电，峰谷差，日平均
-data = pd.DataFrame({
-    'Day_Max':[],
-    'Day_Min':[],
-    'Max-Min':[],
-    'Average':[],
-})
-
-
-'''
-
+    data_new.loc['日均值',_add] = data['Aggregate'].mean()
+    data_new.loc['日峰值',_add] = data['Aggregate'].max()
+    data_new.loc['日谷值',_add] = data['Aggregate'].min()
+    data_new.loc['谷峰差',_add] = data_new.loc['日峰值',_add]-data_new.loc['日谷值',_add]
+    data_new.loc['日峰谷差率',_add] = data_new.loc['日谷值',_add]/data_new.loc['日峰值',_add]
+    data_new.loc['最高用电时段',_add] = Max_hour(data)
+print(data_new)
 # 标准化数据
 scaler = StandardScaler()
-data['Aggregate_scaled'] = scaler.fit_transform(data[['Aggregate']])
+data_new = data_new.T
+data_scaled = scaler.fit_transform(data_new)
 
-# 使用KMeans进行聚类
-kmeans = KMeans(n_clusters=3, random_state=42)
-data['cluster'] = kmeans.fit_predict(data[['Aggregate_scaled']])
+# 选择聚类算法（K-means）
+kmeans = KMeans(n_clusters=4, random_state=0)
+data_new['cluster'] = kmeans.fit_predict(data_scaled)
 
-# 可视化聚类结果
-plt.figure(figsize=(12, 6))
-plt.scatter(pd.to_datetime(data['Unix']), data['Aggregate'], c=data['cluster'], cmap='viridis')
-plt.title('House Electricity Usage Clustering')
-for i in range(2):
-    data = pd.read_csv(csv_files[i])
-    data, data_new = Data_preprocessing(data)
-    data_list.append(data_new)
-Data_Visualize(data_list)
+# 聚类结果可视化
+plt.figure(figsize=(10, 6))
+plt.scatter(data_scaled[:, 0], data_scaled[:, 1], c=data_new['cluster'], cmap='viridis', marker='o')
 
-
+for i, txt in enumerate(data_new.index):
+    plt.annotate(txt, (data_scaled[i, 0], data_scaled[i, 1]))
 
 '''
+plt.xlabel('Scaled Peak Value')
+plt.ylabel('Scaled Average Value')
+plt.title('Cluster Analysis of House Electricity Usage')
+#plt.colorbar(label='Cluster')
 '''
-# 标准化数据
-scaler = StandardScaler()
-data['Aggregate_scaled'] = scaler.fit_transform(data[['Aggregate']])
-
-# 使用KMeans进行聚类
-kmeans = KMeans(n_clusters=3, random_state=42)
-data['cluster'] = kmeans.fit_predict(data[['Aggregate_scaled']])
-
-# 可视化聚类结果
-plt.figure(figsize=(12, 6))
-plt.scatter(pd.to_datetime(data['Unix']), data['Aggregate'], c=data['cluster'], cmap='viridis')
-plt.title('House Electricity Usage Clustering')
-plt.xlabel('Time')
-plt.ylabel('Electricity Usage')
-plt.grid(True)
-plt.ylabel('Electricity Usage')
-plt.grid(True)
 plt.show()
-'''
+
+# 打印聚类结果
+print(data_new)
