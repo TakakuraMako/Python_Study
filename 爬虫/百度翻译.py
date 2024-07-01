a09e59237bb6dbcb2e118a9db2ac2f6dc32bb787
TakakuraMako <1361942776@qq.com>
2024-07-01 10:57:34 +0800

    upgrade

--- 爬虫/百度翻译.py
+++ 爬虫/百度翻译.py
@@ -6,9 +6,9 @@ dic = {'kw' : kw}
 response = requests.post(url, data=dic)
 response.encoding = response.apparent_encoding
 response_json = response.json()
-print(response)
+#print(response)
 out = []
-print(f'输入{kw}')
+#print(f'输入{kw}')
 print('输出:')
 for i in range(len(response_json['data'])):
     print(response_json['data'][i]['v'])
--- 爬虫/豆瓣.py
+++ 爬虫/豆瓣.py
@@ -1,5 +1,6 @@
 import requests
-url = 'https://movie.douban.com/chart'
+import re
+url = 'https://movie.douban.com/top250'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 param = {
     'type': 24,
@@ -10,8 +11,13 @@ param = {
 }
 response = requests.get(url=url,params=param,headers=headers)
 response.encoding = response.apparent_encoding
+content = response.text
 # 打印响应状态码和内容
-print("Status Code:", response.status_code)
-with open('douban.html', 'w', encoding='utf-8') as f:
-    f.write(response.text)
+#print("Status Code:", response.status_code)
+obj = re.compile(r'alt="(?P<title>.*?)"'
+                 r'<p class="">.*?<br>(?P<year>\d+)&nbsp;/&nbsp;.*?</p>',re.S)
+result = obj.finditer(content)
+for i in result:
+    print(i.group('title').strip())
+    print(i.group('year').strip())
 response.close()
