4faa7f4220d88b45be9da05dbf58ec0fd172cefc
TakakuraMako <1361942776@qq.com>
2024-07-03 10:21:41 +0800

    upgrade

--- test.py
+++ test.py
@@ -4,48 +4,12 @@ import json
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0',
-    'referer': 'https://book.jd.com/'
+    'token': '3213401d93ad9be6cbd4d5fe788a1eaaf19e9b2f3dea45161c6b934e77e70b60',
+    'cookie': 'zkhanecookieclassrecord=%2C66%2C53%2C; PHPSESSID=paahb3ftbusrnae6oc16u12a57; zkhanmlusername=%D3%A5%B2%D6%DC%D4%D7%D3; zkhanmluserid=7656247; zkhanmlgroupid=1; zkhanmlrnd=Jwv7bCp7mbYgXOjAS7d4; zkhanmlauth=15838654bb92530b8c22a4ef8e561bde'
 }
-url = 'https://gw-e.jd.com/client.action'
-books_info = []
+url = 'https://pic.netbian.com/e/extend/downpic.php?'
 
-for i in range(1, 6):
-    body = json.dumps({"moduleType": 1, "page": i, "pageSize": 20, "scopeType": 1})  # JSON格式的字符串作为请求参数
-    params = {
-        "callback": "func",
-        "body": body,
-        "functionId": "bookRank",
-        "client": "e.jd.com",
-    }
-    resp = requests.get(url=url, params=params, headers=headers)
-    
-    # 移除JSONP回调函数
-    jsonp_text = resp.text
-    json_text = re.search(r'func\((.*)\)', jsonp_text).group(1)
-    
-    # 解析JSON数据
-    data = json.loads(json_text)
-    
-    # 获取 'data' 部分
-    books = data['data']['books']
-    for book in books:
-        book_id = book['bookId']
-        book_name = book['bookName']
-        publisher = book['publisher']
-        item_url = f'https://item.jd.com/{book_id}.html'
-        sell_price = book['sellPrice']
-        define_price = book['definePrice']
-        
-        # 确保所有字段为字符串
-        book_id = str(book_id)
-        book_name = str(book_name)
-        publisher = str(publisher)
-        item_url = str(item_url)
-        sell_price = str(sell_price)
-        define_price = str(define_price)
-        
-        books_info.append((book_id, book_name, sell_price, define_price, publisher, item_url))
-
-# 打印获取的书籍信息
-for book in books_info:
-    print(book)
+resp = requests.get(url=url, headers=headers)
+print(resp.text)
+with open('test.jpg','wb') as f:
+    f.write(resp.content)
--- 爬虫/风景.py
+++ 爬虫/风景.py
@@ -1,17 +1,20 @@
 import requests
 import re
-url = 'https://pic.netbian.com/4kfengjing/'
+url = 'https://pic.netbian.com/4kfengjing/index'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
-
-resp = requests.get(url = url, headers=headers)
-resp.encoding = resp.apparent_encoding
 obj = re.compile(r'<li><a.*?><img\ssrc="(?P<img>.*?)".*?"(?P<name>.*?)"')
-result = obj.finditer(resp.text)
-n = 1
-for i in result:
-    pic = requests.get(url='https://pic.netbian.com'+i.group('img').strip())
-    _ = i.group('name').strip()
-    print(_)
-    with open(f'./爬虫/picture/{n}.jpg','wb') as f:
-        f.write(pic.content)
-    n += 1
+for page in range(1, 4):#只爬前三页
+    if page == 1:
+        resp = requests.get(url = url + '.html', headers=headers)
+    else:
+        resp = requests.get(url = url + f'_{str(page)}.html', headers=headers)
+    resp.encoding = resp.apparent_encoding
+    result = obj.finditer(resp.text)
+    n = 1
+    for i in result:
+        pic = requests.get(url='https://pic.netbian.com'+i.group('img').strip())
+        _ = i.group('name').strip()
+        print(_)
+        with open(f'./爬虫/picture/{page}_{n}.jpg','wb') as f:
+            f.write(pic.content)
+        n += 1
