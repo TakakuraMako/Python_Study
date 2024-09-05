acdf81856b4dffcba7cf91bbe6a30df857510e6e
TakakuraMako <1361942776@qq.com>
2024-09-05 19:34:51 +0800

    upgrade

--- 问题一/品种单价变化.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = pd.read_excel('附件3.xlsx')
-
-# Extract the relevant columns
-new = data.groupby(['日期', '分类名称'])['批发价格(元/千克)'].mean().reset_index()
-# Save the filtered data to a new Excel file
-output_file_path = '批发随日期变化.xlsx'
-new.to_excel(output_file_path, index=False)
-
