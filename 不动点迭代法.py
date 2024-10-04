219481eb2961b505b4cf7400a9531772257cab39
TakakuraMako <1361942776@qq.com>
2024-10-04 17:00:16 +0800

    upgrade

--- 不动点迭代法.py
+++ 不动点迭代法.py
@@ -1,11 +1,9 @@
 from sympy import *
 import decimal
-import numpy as np
 x = symbols('x')
 a = decimal.Decimal(1)
 b = decimal.Decimal(2)
 accuracy = decimal.Decimal(2**-5)
-f_x = x**3 + 4*x**2 -10
 phi_x = 1/2*(10 - x**3)**(1/2)
 x_old = (a+b)/2
 count = 0
