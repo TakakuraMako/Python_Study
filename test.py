62c4e94e7d8900a60f06fd02f3e9a0145e5d8c10
TakakuraMako <1361942776@qq.com>
2024-11-12 20:36:58 +0800

    upgrade

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 import decimal
+import math
 from sympy import *
-x, y = symbols('x y')
-f = y *Rational(8,3) * x * (x-1) * (x-2) - 6 * x * (x-0.5) * (x-2) + Rational(2,3) * x * (x-0.5) * (x-1)
-g = expand(f)
-g = nsimplify(g)
-print(g) 
+print(math.degrees(math.atan(1)))
