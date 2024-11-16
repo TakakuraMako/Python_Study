5e3d0522684aff6ead0332c6411b548be29370b8
TakakuraMako <1361942776@qq.com>
2024-11-16 14:49:29 +0800

    upgrade

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import decimal
 import math
 from sympy import *
-list1 = list(range(2000, 2017, 1))
-print(list1)
+import numpy as np
+phi = np.matrix([[1/2, -1],[-1/4, 1/2]])
+L = np.matrix([[1, -2], [1/2, 1]])
+print(phi, L, sep='\n')
+A = np.dot(L, phi)
+print(L**-1)
+print(np.dot(A, L**-1))
