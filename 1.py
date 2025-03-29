299a8751595f2e43b23ee981338413950fd82180
TakakuraMako <1361942776@qq.com>
2025-03-29 22:21:03 +0800

    upgrade

--- 1.py
+++ 1.py
@@ -4,15 +4,44 @@ import matplotlib.pyplot as plt
 # 使plt支持中文
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-data = pd.DataFrame(np.array([[184, 207, 236, 262, 284, 311, 354, 437, 485, 550, 693, 762, 803, 896, 1070, 1331, 1746, 2336, 2641, 2834, 2972, 3180],
-                     [138, 158, 178, 199, 221, 246, 283, 347, 376, 417, 508, 553, 571, 621, 718, 855, 1118, 1434, 1768, 1876, 1895, 1973],
-                     [405, 434, 496, 562, 576, 603, 662, 802, 920, 1089, 1431, 1568, 1686, 1925, 2356, 3027, 3891, 4874, 5430, 5796, 6217, 6651]]).T, index=range(1978, 2000), columns=['全国居民', '农村居民', '城镇居民'])
+data1 = pd.DataFrame([[35.22, 499.80], [10.41, 161.37], [17.22, 273.29], [10.70, 134.79],
+                      [10.29, 90.92], [18.66, 348.99], [4.41, 106.89], [6.24, 196.44],
+                      [49.72, 656.95], [47.70, 580.70], [36.55, 518.10], [14.85, 179.41],
+                      [19.46, 250.16], [10.93, 122.06], [40.26, 552.74], [19.82, 268.20],
+                      [19.49, 221.43], [16.01, 197.68], [99.32, 1080.26],[14.77, 160.60],
+                      [3.96, 39.51], [10.49, 111.76], [21.71, 250.09], [13.06, 95.87],
+                      [20.34, 183.62], [0.77, 6.08], [11.38, 133.50], [3.66, 64.86],
+                      [1.21, 18.30],[2.31, 23.81], [3.24, 103.81]], columns=['11月', '1~11月'], index=range(1, 32))
+x1_mean = round(data1.mean(), 2)
+S1_2 = round(np.var(data1, ddof=1, axis=0), 2)
+S1 = round(np.std(data1, ddof=1, axis=0), 2)
+CV1 = round(x1_mean / S1, 2)
+G31 = round(data1.skew(), 2)
+G41 = round(data1.kurt(), 2)
+M1 = data1.median()
+Q11 = data1.quantile(0.25)
+Q31 = data1.quantile(0.75)
+IQR1 = Q31 - Q11
+TM1 = 1/4*Q11 + 1/2*M1 + 1/4*Q31
+# 画出经验分布函数图
+# 计算经验分布函数的值
+def empirical_distribution_function(data):
+    n = len(data)
+    sorted_data = np.sort(data)
+    
+    # 计算经验分布函数
+    ecdf = np.arange(1, n + 1) / n
+    return sorted_data, ecdf
 
-# 每一列分为10组，分别做直方图
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-for i, col in enumerate(data.columns):
-    data[col].plot(kind='hist', bins=10, ax=axes[i], title=col)
-    axes[i].set_xlabel('频数')
-    axes[i].set_xlim(0, 7000)
+# 获取排序的数据和对应的经验分布函数值
+sorted_data, ecdf = empirical_distribution_function(data1.iloc[:, 0])
 
+# 绘制经验分布函数曲线
+plt.figure(figsize=(10, 6))
+plt.step(sorted_data, ecdf, where='post', label='Empirical CDF', color='blue')
+plt.title('Empirical Distribution Function (EDF)')
+plt.xlabel('Data Value')
+plt.ylabel('Empirical Probability')
+plt.grid()
+plt.legend()
 plt.show()
