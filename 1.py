7eeb75d0e708776fb29511d35564357b8534e3f2
TakakuraMako <1361942776@qq.com>
2025-04-02 23:27:11 +0800

    upgrade

--- 1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import torch
-
-input = torch.tensor([[1, -0.5], [-1, 3]])
-a = torch.reshape(input, (1, 1, 2, 2))
-b = torch.reshape(input, (-1, 1, 2, 2))
-print(a)
-print(b)
