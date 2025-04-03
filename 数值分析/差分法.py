ee9d2f7b3ea4e620636c7abdfe86be06dd3a7244
TakakuraMako <1361942776@qq.com>
2025-04-03 19:40:36 +0800

    upgrade

--- 数值分析/差分法.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-fx = np.array([])
-def diff_fun(fx):
-    result = np.array([])
-    if len(fx) == 1:
-        
-    for i in range(len(fx) - 1):
-        result = np.concatenate(result, fx[i+1] - fx[i])
